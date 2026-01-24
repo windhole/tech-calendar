47172061c345e137ea13b930f5696cf2a7afeb4c
Ko Kazaana <4735171+windhole@users.noreply.github.com>
2026-01-24 10:26:35 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/tech-calendar/',
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
